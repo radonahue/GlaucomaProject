9dc87f4b4d1c8e7aefff1d0243795c183d0d2fba
Rachel Donahue <radona@bu.edu>
2022-04-27 16:02:27 -0400

    Some graph updates

--- EDAScript.R
+++ EDAScript.R
@@ -1,6 +1,6 @@
 library(openxlsx)
 library(ggplot2)
-library(d)
+library(dplyr)
 data<-read.xlsx("Data.xlsx")
 View(data)
 
@@ -8,13 +8,19 @@ View(data)
 #https://www.webmd.com/eye-health/how-read-eye-glass-prescription#:~:text=When%20you%20look%20at%20your,means%20something%20involving%20both%20eyes.
 
 #Quotes from client email about variable importance:
-"I think the factors that are important to consider when examining patients that had IOP increases after SLT and ultimately had a subsequent surgical intervention or patients that while not requiring a surgery had subsequent decline on their HVF or OCT parameters would be: Age at SLT, Pre-VA, Pre-Refraction, Latest Pre-IOP,  CCT, Pre-HVF MD and PSD, and Pre-OCT Mean, Sup, and Inf Thicknesses. Secondary factors would be: Ethnicity (might not have a varied enough sample), Gender, Eye, and PMH (diabetes and hypertension). Not sure I have enough patients with each Glaucoma Dx to really see differences. "
+# "I think the factors that are important to consider when examining patients that had IOP increases after SLT and ultimately had a subsequent surgical intervention or patients that while not requiring a surgery had subsequent decline on their HVF or OCT parameters would be: Age at SLT, Pre-VA, Pre-Refraction, Latest Pre-IOP,  CCT, Pre-HVF MD and PSD, and Pre-OCT Mean, Sup, and Inf Thicknesses. Secondary factors would be: Ethnicity (might not have a varied enough sample), Gender, Eye, and PMH (diabetes and hypertension). Not sure I have enough patients with each Glaucoma Dx to really see differences. "
 
 
-d<-duplicated(data$PatientID2)
+DuplicationIndicator<-duplicated(data$PatientID2)
 data<-cbind(data, d)
 
-ggplot(data, aes(x=Age.at.SLT, fill=d))+geom_bar()+  theme(legend.position = "none")+ggtitle("Initial SLT Ages with Duplicate Patients")
+data$PatientID2<-data$Patient.ID
+test<-data%>%group_by(PatientID2)%>%summarize(count=n())
+data<-left_join(data, test, by="PatientID2")
+d2<-distinct(data, PatientID2, count, Age.at.SLT)
+
+
+ggplot(d2, aes(x=Age.at.SLT, fill=as.factor(count)))+geom_bar()+ggtitle("Initial SLT Ages with Duplicate Patients")
 
 ggplot(data, aes(x=Ethnicity, fill=Gender))+geom_bar()+ggtitle("Ethnicity of Patients") 
 
