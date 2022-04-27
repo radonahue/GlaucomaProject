f6190518d8abc77d3c03f76e4d11469a4ccdba04
Rachel Donahue <radona@bu.edu>
2022-04-27 14:21:45 -0400

    Starting an EDA Script

---
+++ EDAScript.R
@@ -0,0 +1,8 @@
+library(openxlsx)
+library(ggplot2)
+data<-read.xlsx("Data.xlsx")
+View(data)
+
+#OS is left eye OD is right eye:
+#https://www.webmd.com/eye-health/how-read-eye-glass-prescription#:~:text=When%20you%20look%20at%20your,means%20something%20involving%20both%20eyes.
+
